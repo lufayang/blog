c339ee447662114154bf06ea8b4afe69e7248993
Jung Yang <lufayang@gmail.com>
2022-01-30 20:55:29 -0700

    docs

---
+++ docs/assets/js/search-data.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "placeholder title",
+        "excerpt":"placeholder content ","categories": ["random"],
+        "tags": [],
+        "url": "http://localhost:4000/random/2022/01/23/wordles-subtle-delights.html"
+      }]
